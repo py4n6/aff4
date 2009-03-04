d3a04f58a1b2a6bacd2319dd5b457ba5fceccab7
Bradley Schatz <bradley@schatzforensic.com.au>
2009-03-04 16:16:51 +1000

    remove-container-depends Ignore-this: d4f971f307123e3d711359aa98fa8b4f

--- tests/testOpen.py
+++ tests/testOpen.py
@@ -1,24 +1,47 @@
 import fif
-import unittest
+import unittest, os
 
 class TestOpen(unittest.TestCase):
 
     def setUp(self):
-        pass
+        # create a basic container for these tests
+        fiffile = fif.FIFFile()
+        fiffile.create_new_volume("image.zip")
+        stream = fiffile.create_stream_for_writing(stream_type = "aff2-storage:Image",
+                                                  local_name = "fooimage.dd")
 
+        # create an information storage stream to store the source information
+        infostream = fiffile.create_stream_for_writing(stream_type = "aff2-storage:Info")
+
+        stream.properties["aff2-storage:streamSource"] = infostream.getId()
+        infostream.properties["aff2-storage:type"] = "aff2-info:File"
+        infostream.properties["aff2-storage:path"] = "badpath"
+        fiffile.properties["aff2-storage:containsImage"] = stream.getId()
+
+        data = "foobar"
+        stream.write(data)
+        stream.close()
+        infostream.close()
+        fiffile.close()
+
+        
     def testOpenLocalImage(self):
-        fiffile = fif.FIFFile("../samples/mediumimage.00.zip")
-        fd = fiffile.open_stream_by_name("mediumimage.dd")
+        fiffile = fif.FIFFile("image.zip")
+        fd = fiffile.open_stream_by_name("fooimage.dd")
         self.assertNotEqual(fd, None)
+        fiffile.close()
 
     def testOpenLocalImageWithBadNameFails(self):
-        fiffile = fif.FIFFile("../samples/mediumimage.00.zip")
+        fiffile = fif.FIFFile("image.zip")
         self.assertRaises(IOError, fiffile.open_stream_by_name, "badname.dd")
+        fiffile.close()
         
     def testOpenLocalImageWithBadFileNameFails(self):
         self.assertRaises(IOError, fif.FIFFile, "../samples/nonexistentimage.00.zip")
-      
-       
+
+    def tearDown(self):
+        os.unlink("image.zip")
+    
         
 def suite():
     suite = unittest.TestLoader().loadTestsFromTestCase(TestOpen)
